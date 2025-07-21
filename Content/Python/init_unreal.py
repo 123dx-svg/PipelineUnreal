d1c523570733dde97fd16439864eff3dc930d8d5
dingxiao <11756@DESKTOP-2USBMQO>
2025-07-21 20:10:39 +0800

    批量导入时文件夹冲突问题

--- Content/Python/init_unreal.py
+++ Content/Python/init_unreal.py
@@ -1,5 +1,5 @@
 from post_import_callbacks import register_import_callback
 print("===EXECUTING Content/Python init unreal")
-#初始文件记录导入规则
-register_import_callback()
+#初始文件记录导入规则 暂时回合其他导入规则冲突，他会根据元数据导入模型时自动匹配文件夹自动执行
+#register_import_callback()
 
