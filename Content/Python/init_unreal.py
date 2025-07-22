b848ac800e619e0811e6886effe1a060d9a1ac33
dingxiao <11756@DESKTOP-2USBMQO>
2025-07-22 23:55:55 +0800

    贴图大小自动验证，目前存在错误日志无法输出问题暂时使用警告日志

--- Content/Python/init_unreal.py
+++ Content/Python/init_unreal.py
@@ -4,4 +4,4 @@ print("===EXECUTING Content/Python init unreal")
 #初始文件记录导入规则 暂时回合其他导入规则冲突，他会根据元数据导入模型时自动匹配文件夹自动执行
 #register_import_callback()
 #验证前缀
-register_asset_prefix_validator()
+#register_asset_prefix_validator()
